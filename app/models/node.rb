58f3ceba852279857f54d735ad8c92aeafeba84f
yanosz <yanosz@gmx.net>
2012-05-28 19:02:43 +0200

    Fix timezone mismatch

--- app/models/node.rb
+++ app/models/node.rb
@@ -46,7 +46,11 @@ class Node < ActiveRecord::Base
           node_mac = md[2]
           node_ip = md[3]
           md2 = time_stmp.match '(\w+) (\d\d) (\d\d):(\d\d):(\d\d)'
-          yet.month = md2[1]; yet.day = md2[2]; yet.hour = md2[3]; yet.minute = md2[4]; yet.sec = md2[5]
+          yet = yet.change(:month => md[1], 
+            :day => md2[2],
+            :hour => md2[3],
+            :minute => md2[4],
+            :second => md2[5] )
           # Since tinc tries to connect every 45secs, we will use data younger than 45secs only
           ago = yet - t45_secs_ago #If ago > 0 => Time > t45_secs_ago => Recent enough
           if(ago > 0 || historic) # If recent enough or historic nodes should be included ...
