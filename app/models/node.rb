ef3874b2dabf1d52f45556e7225b32108965f3d2
yanosz <yanosz@gmx.net>
2012-05-28 19:19:27 +0200

    Show all non-registered nodes

--- app/models/node.rb
+++ app/models/node.rb
@@ -13,12 +13,8 @@ class Node < ActiveRecord::Base
   end
   
   def self.all_unregistered
-    running_nodes = Node.where(:status_id => Status.up, :user_id => nil) || []
-    connecting_nodes = Node.unregistred(true) || []
-    return running_nodes + connecting_nodes 
+    running_nodes = Node.where(:user_id => nil) || []
   end
-    
-
 
   def current_status
     self.status || Status.find_by_name("down")
@@ -48,7 +44,6 @@ class Node < ActiveRecord::Base
           yet = Time.local(t45_secs_ago.year, md2[1], md2[2],md2[3],md2[4],md2[5])
           # Since tinc tries to connect every 45secs, we will use data younger than 45secs only
           ago = yet - t45_secs_ago #If ago > 0 => Time > t45_secs_ago => Recent enough
-          logger.info "ago (#{ago}) = #{yet} - #{t45_secs_ago}"
           if(ago > 0 || historic) # If recent enough or historic nodes should be included ...
             nodes[node_mac] = Node.new(:wlan_mac => node_mac, :bat0_mac => node_mac, :current_ip => node_ip, :updated_at => DateTime.parse(yet.to_s))
           end
